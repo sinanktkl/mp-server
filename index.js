e26c404db9dbacfd34fc789fb99231c0c66ceeb1
sinanktkl <muhammedsinan000001@gmail.com>
2024-11-12 02:17:31 +0530

    mp-server

---
+++ index.js
@@ -0,0 +1,14 @@
+const JSONServer = require('json-server')
+const MpServer = JSONServer.create()
+const router = JSONServer.router('db.json')
+const middleware = JSONServer.defaults()
+
+const PORT = 3000 || process.env.PORT
+
+MpServer.use(middleware)
+MpServer.use(router)
+
+MpServer.listen(PORT,()=>{
+    console.log(`Media Player Server Started running at Port:${PORT} & waiting for the client Request `);
+    
+}) 
